89babf5ad372c0848a82b67695bcee49251a344d
Abdullahbotxp <132236513+Abdullahbotxp@users.noreply.github.com>
2023-05-01 14:39:56 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,33 +5,33 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'LuciferMoringstar_Robot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+SESSION = 'LuciferMoringstar_Robot'
+API_ID = 29514095
+API_HASH = 'c17fef5596d69ef2910d3f74e743eb73'
+BOT_TOKEN = '6133107220:AAGHQrtyuuj_sCICm7BQAiExsukbWwGisgE'
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
 
-BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", ""))
-ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
-DB_URL = os.environ.get("DATABASE_1", "")
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
+BROADCAST_CHANNEL = -1001964286309
+ADMIN_ID = 5071390190
+DB_URL = 'mongodb+srv://BOTXP:BOTXP@cluster0.lvvmb9h.mongodb.net/?retryWrites=true&w=majority'
+BROADCAST_AS_COPY = True
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('FORCES_SUB')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
+ADMINS = 5071390190
+CHANNELS = -1001901200484
+auth_users = []
+AUTH_USERS = []
+auth_channel = -1001416467235
+AUTH_CHANNEL = -1001416467235
+AUTH_GROUPS = None
 TUTORIAL = "https://youtu.be/5hnYOKBzyi8"
 # MongoDB information
-DATABASE_URI = environ['DATABASE_2']
-DATABASE_NAME = environ['BOT_NAME']
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = "mongodb+srv://BOTXP:BOTXP@cluster0.lvvmb9h.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'LuciferMoringstar_Robot'
+COLLECTION_NAME = 'Telegram_files'
 
 # Messages
 default_start_msg = """
@@ -42,7 +42,7 @@ Here you can search files in Inline mode as well as PM, Use the below buttons to
 START_MSG = environ.get('START_MSG', default_start_msg)
 
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
-OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
+OMDB_API_KEY = 'e882d08a'
 if FILE_CAPTION.strip() == "":
     CUSTOM_FILE_CAPTION=None
 else:
